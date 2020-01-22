e7e6c441915ae1c86bb855fca87b4e23ac456944
Souza <debora.souza@wunderman.com>
2020-01-22 11:04:45 -0300

    Adiciona novos icones, fontes e estilo

--- src/components/Compass.vue
+++ src/components/Compass.vue
@@ -1,20 +1,33 @@
 <template>
-  <div class="compass-component">
-    <div class="container">
-      <input type="text" v-model="search" placeholder="Digite sua cidade aqui" autocomplete="off" @input="filterCities" @focus="modal = true"  class="compass-search">
-      <div v-if="modal && search !== ''">
-        <div class="compass-list-center">
-          <ul class="compass-cities">
-            <li v-for="(filteredCity, i) in filteredCities" :key="i" @click="setCity(filteredCity)" class="compass-city">{{ filteredCity }}</li>
-          </ul>
-        </div>
-      </div>
+  <div class="compass">
+    <div class="compass__content compass__container">
+      <a class="compass__content__link only-desktop">
+        <p @click="switchModalOn()">Qual a sua localização?</p>
+        <img src="../assets/images/icons/icon_location.svg" alt="">
+      </a>
+        <div class="compass__model__input">
+          <span class="icon-close">
+            <img src="../assets/images/icons/close.svg" alt="Ícone Fechar">
+          </span>
+          <form>
+            <input type="text" v-model="search" placeholder="Informe a sua cidade" autocomplete="off" @input="filterCities" @focus="modal = true" id="autocomplete_input">
+            <div v-if="modal && search !== ''">
+              <div class="compass__list-center">
+                <ul class="compass__cities">
+                  <li v-for="(filteredCity, i) in filteredCities" :key="i" @click="setCity(filteredCity)" class="compass__city">{{ filteredCity }}</li>
+                </ul>
+              </div>
+            </div>
+          </form>
+        </div> 
     </div>  
   </div>
 </template>
 
 <script>
 import cities from '../data/cities.json'
+import "../styles/main.scss";
+
 export default {
   data() {
     return {
@@ -38,9 +51,8 @@ export default {
     },
     filterCities() {
        this.filteredCities = this.cidades.filter(cidade => {
-         return cidade.toLowerCase().startsWith(this.search.toLowerCase());
-       })
-    
+         return cidade.toLowerCase().startsWith(this.search.toLowerCase())
+       }) 
     },
     setCity(city) {
       this.search = city
@@ -48,46 +60,106 @@ export default {
       
       this.modal = false
       localStorage.setItem("cidade", city);
-    } 
+    },
+    switchModalOn() {
+      $('.compass__content__link').hide()
+      $('.compass__model__input').show()
+      $("#autocomplete_input").focus()
+    }
+    
   }
 }
 </script>
 
 <style lang="scss" scoped>
 
-  .compass-component .container {
-    margin: auto;
-    max-width: 960px;
-    padding: 0 10px; 
-  }
-
-  .compass-component .compass-search {
-    background-color: #d3d3de;
-    padding: 10px 10px;
-    height: 25px;
-    border: none;
-    width: 25%;
-  }
-
-  .compass-component .compass-cities {
-    background-color: #35495e;
-    color: #fff;
-    text-decoration: none;
-    margin: 0;
-    padding: 0;
-    width: 27%;
-  }
-
-  .compass-component .compass-city {
-    list-style-type: none;
-    border-bottom: 1px solid #fff;
-    padding: 10px 45px;
-  }
   
-  .compass-component .compass-list-center {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+
+  * {
+    font-family: roboto;
+  }
+
+  .compass {
+
+    &__container {
+      margin-right: auto; 
+      margin-left:  auto; 
+      max-width: 300px; 
+    }
+
+    &__cities {
+      color: #000;
+      border: 1px solid #999;
+      text-decoration: none;
+      margin: 0;
+      padding: 0;
+      width: 100%;
+      margin-top: -3px;
+      border-top: 0px !important;
+    }
+
+    &__city {
+      list-style-type: none;
+      border-bottom: 1px solid #999;
+      padding: 15px 45px;
+      transition: color ease-in-out .1s;
+
+      &:hover {
+        color: #609;
+        cursor: pointer;
+      }
+    }
+ 
+    &__list-center {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    &__content__link {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+
+      p {
+        font-weight: 600;
+        color: #609;
+        /* display: inline; */
+      }
+
+      img {
+        width: 22px;
+        margin-left: 2px;
+        height: 22px;
+      }
+    }
+    &__model__input {
+      display: none;
+      width: 100%;
+      position: relative;
+
+      input {
+      padding-left: 10px;
+      height: 48px;
+      border-radius: 5px 5px 0px 0px;
+      border: 1px solid #999;
+      width: 100%;
+      font-size: 14px;
+      /* display: none;  */
+
+      &:focus {
+        outline: none;
+      }
+    }
+
+      .icon-close {
+        position: absolute;
+        right: 10px;
+        top: 15px;
+        cursor: pointer;
+      }  
+    }
   }
   
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import jQuery from 'jquery'
 
 Vue.config.productionTip = false
+
+window.$ = jQuery
+window.jQuery = jQuery
 
 new Vue({
   render: h => h(App),
